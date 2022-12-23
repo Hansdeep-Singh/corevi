83a88e9d06b89d35c4976036003c01663f32bb9f
Hansdeep Singh <hansdeep.singh@hotmail.com>
2022-12-23 12:36:31 +1100

    Library being used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
+import { MenuComponent } from './menu/menu.component';
+
+import { HansLibModule } from 'hans-lib';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HansLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
