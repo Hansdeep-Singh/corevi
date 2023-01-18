a354ab059116a919fbc7a959cb0db457956525df
Hansdeep Singh <hansdeep.singh@hotmail.com>
2023-01-19 09:51:51 +1100

    wip

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EngineService, INotifyConfig, UtilitiesService } from 'hans-lib';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  message: INotifyConfig | undefined;
+  constructor(private utilityService: UtilitiesService, private engineService: EngineService) { }
 
   ngOnInit(): void {
+  }
+
+  registerForm = new FormGroup({
+    emailAddress: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+  validForm() {
+    this.message = { success: false, notifyMessage: 'Please complete the form' };
+    this.registerForm.invalid ? this.engineService.changeNotifyMessage(this.message) : "";
+  }
+  submit(post: any) {
   }
 
 }
